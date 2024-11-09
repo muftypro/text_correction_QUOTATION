7c25e3fe41edec89f11a7050ffa8291238c88468
muftY <61664857+muftypro@users.noreply.github.com>
2024-11-09 14:19:41 +0600

    Add files via upload

---
+++ QUOTAION to QUOTATION CORRECTION Text-1.1.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Edit Car Edit Page Text
+// @namespace    http://tampermonkey.net/
+// @version      1.1
+// @description  Change specific text on car edit page
+// @author       Your Name
+// @match        https://salsabeelcars.site/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const newText = "<strong>OFFER/</strong><br>QUOTATION"; // HTML formatted text
+    const targetSelector = 'body > div.wrapper > div.content-wrapper > section > div > div > div.panel-body > div:nth-child(1) > div > div.col-sm-3.top_left > p';
+
+    const observer = new MutationObserver((mutationsList, observer) => {
+        const targetElement = document.querySelector(targetSelector);
+        if (targetElement) {
+            targetElement.innerHTML = newText; // Use innerHTML to allow bold and line break
+            observer.disconnect();
+        } else {
+            console.error('Target element not found.');
+        }
+    });
+
+    observer.observe(document.body, { childList: true, subtree: true });
+})();
